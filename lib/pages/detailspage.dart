07934b245fdf4448f276c7a4ae3031589fcc2d0a
Pizzacorn.es <hola@pizzacorn.es>
2025-02-11 11:02:28 +0100

    jodsnajds

--- lib/pages/detailspage.dart
+++ lib/pages/detailspage.dart
@@ -19,14 +19,23 @@ class DetailPage extends StatelessWidget {
         color: COLOR_BACKGROUND,
         child: ListView(
           children: [
+
             ImagenWidget(),
+
             DescripcionWidget(),
+
             EspacioFull(size: 10),
+
             HorarioWidget(),
+
             EspacioFull(size: 20),
+
             RutaWidget(),
+
             EspacioFull(size: 20),
+
             BotonreservaWidget(),
+
           ],
         ),
       ),

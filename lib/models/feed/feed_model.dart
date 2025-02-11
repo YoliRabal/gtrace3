a93dbffa2bf397f13c9b5d4ef8913784fb4fddd0
YoliRabal <yoli.rabal.adan@gmail.com>
2025-02-11 10:52:47 +0100

    Nuevo proyecto

---
+++ lib/models/feed/feed_model.dart
@@ -0,0 +1,53 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FeedModel {
+  String id;
+  String titulo;
+  String descripcion;
+  String ubicacion;
+  String fecha;
+  String categoria;
+  String imagen;
+  String organizador;
+  String organizadorImagen;
+
+  // Constructor con valores predeterminados
+  FeedModel({
+    this.id = '',
+    this.titulo = '',
+    this.descripcion = '',
+    this.ubicacion = '',
+    this.fecha = '',
+    this.categoria = '',
+    this.imagen = '',
+    this.organizador = '',
+    this.organizadorImagen = '',
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'titulo': titulo,
+      'descripcion': descripcion,
+      'ubicacion': ubicacion,
+      'fecha': fecha,
+      'categoria': categoria,
+      'imagen': imagen,
+      'organizador': organizador,
+      'organizadorImagen': organizadorImagen,
+    };
+  }
+
+  factory FeedModel.fromJson(Map<String, dynamic> map, String documentId) {
+    return FeedModel(
+      id: documentId,
+      titulo: map['titulo'] ?? '',
+      descripcion: map['descripcion'] ?? '',
+      ubicacion: map['ubicacion'] ?? '',
+      fecha: map['fecha'] ?? '',
+      categoria: map['categoria'] ?? '',
+      imagen: map['imagen'] ?? '',
+      organizador: map['organizador'] ?? '',
+      organizadorImagen: map['organizadorImagen'] ?? '',
+    );
+  }
+}
--- lib/widgets/feed_widget.dart
+++ lib/widgets/feed_widget.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+FirebaseFirestore firestore = FirebaseFirestore.instance;
 
 class FeedWidget extends StatelessWidget {
   const FeedWidget({super.key});
